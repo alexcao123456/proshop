dd9d382dd112ea17a346d65c9b4f00f68041c1cd
alexcao <alexcao12138@163.com>
2024-10-21 14:46:44 +0800

    make the images the same size

--- frontend/src/component/Product.jsx
+++ frontend/src/component/Product.jsx
@@ -7,7 +7,11 @@ const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/product/${product._id}`}>
-        <Card.Img variant="top" src={product.image} />
+        <Card.Img
+          variant="top"
+          src={product.image}
+          style={{ width: "100%", height: "200px", objectFit: "cover" }}
+        />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
